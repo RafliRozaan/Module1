26c1635d199ab57a0956a627cf4ed49b161ad375
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-06 03:35:40 +0800

    Update Input.py

--- pages/Input.py
+++ pages/Input.py
@@ -1,8 +1,204 @@
 import pandas as pd
-from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import os
+import numpy as np
+import tensorflow as tf
+from sklearn.cluster import KMeans
+import cv2
+from PIL import Image   
+import dataiku
+
+#Preprocessing Lib Start
+
+def random_color(C):
+    # C is an integer that represents the number of colors to output
+    # The function returns a list of C random and distinct RGB colors
+    # The colors are not too white (i.e. the sum of RGB values is less than 600)
+    colors = [] # an empty list to store the colors
+
+    while len(colors) < C: # loop until we have C colors
+        r = np.random.randint(0, 255) # generate a random red value
+        g = np.random.randint(0, 255) # generate a random green value
+        b = np.random.randint(0, 255) # generate a random blue value
+        color = (r, g, b) # create a tuple for the color
+        if sum(color) < 600 and color not in colors: # check if the color is not too white and not already in the list
+            colors.append(color) # add the color to the list
+    return colors # return the list of colors
+
+def rgb_to_hex(rgb):
+    """Converts a tuple of RGB values to a hexadecimal color code.
+
+    Parameters:
+        rgb (tuple): A tuple of three integers in the range 0-255 representing the red, green and blue components of the color.
+
+    Returns:
+        str: A string of six hexadecimal digits representing the color code.
+
+    Example:
+        >>> rgb_to_hex((255, 0, 0))
+        'FF0000'
+    """
+    # Check if the input is a valid RGB tuple
+    if not isinstance(rgb, tuple) or len(rgb) != 3 or not all(0 <= x <= 255 for x in rgb):
+        raise ValueError("Invalid RGB input")
+
+    # Convert each component to a two-digit hexadecimal string
+    hex_components = [format(x, "02X") for x in rgb]
+
+    # Join the components and return the result
+    return "".join(hex_components)
+
+def generate_tuples(N):
+    result = []
+    for i in range(N):
+        result.append(((255-i)/255, 0, 0))
+    return result
+
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+
+def resize(img,k):
+    return cv2.resize(img, dsize=(int(img.shape[1]*k), int(img.shape[0]*k)), interpolation=cv2.INTER_NEAREST)
+
+
+def crop_image_v2(image, crop_size, pixel_value):
+  # Get the shape of the image
+  height, width, N = image.shape
+  # Get the crop height and width
+  crop_height, crop_width = crop_size
+  # Calculate the number of crops along each dimension
+  num_crops_h = height // crop_height + (height % crop_height != 0)
+  num_crops_w = width // crop_width + (width % crop_width != 0)
+  # Calculate the total number of crops
+  num_crops = num_crops_h * num_crops_w
+  # Initialize the output array with the padding value
+  output = np.full((num_crops, crop_height, crop_width, N), pixel_value)
+  # Loop over the crops and copy the image data
+  for i in range(num_crops_h):
+    for j in range(num_crops_w):
+      # Get the index of the current crop
+      k = i * num_crops_w + j
+      # Get the start and end coordinates of the image region to copy
+      start_y = i * crop_height
+      end_y = min((i + 1) * crop_height, height)
+      start_x = j * crop_width
+      end_x = min((j + 1) * crop_width, width)
+      # Get the start and end coordinates of the output region to paste
+      out_start_y = 0
+      out_end_y = end_y - start_y
+      out_start_x = 0
+      out_end_x = end_x - start_x
+      # Copy the image data to the output array
+      output[k, out_start_y:out_end_y, out_start_x:out_end_x, :] = image[start_y:end_y, start_x:end_x, :]
+  # Return the output array and the number of crops along each dimension
+  return output, (num_crops_h, num_crops_w)
+
+def uncrop_image_v2(cropped_images, num_crops):
+  # Get the shape of the cropped images
+  num_crops_total, crop_height, crop_width, N = cropped_images.shape
+  # Get the number of crops along each dimension
+  num_crops_h, num_crops_w = num_crops
+  # Calculate the height and width of the original image
+  height = num_crops_h * crop_height
+  width = num_crops_w * crop_width
+  # Initialize the output array with zeros
+  output = np.zeros((height, width, N))
+  # Loop over the cropped images and copy them to the output array
+  for i in range(num_crops_h):
+    for j in range(num_crops_w):
+      # Get the index of the current cropped image
+      k = i * num_crops_w + j
+      # Get the start and end coordinates of the output region to paste
+      start_y = i * crop_height
+      end_y = (i + 1) * crop_height
+      start_x = j * crop_width
+      end_x = (j + 1) * crop_width
+      # Copy the cropped image data to the output array
+      output[start_y:end_y, start_x:end_x, :] = cropped_images[k]
+  # Return the output array as an integer type (to match the original image type)
+  return output.astype(np.uint8)
+
+def predict_curves(img,model):
+    crop_sizes = 32*10
+    c_img, ri = crop_image_v2(img,(crop_sizes,crop_sizes),255)
+    p = model.predict(c_img/255.0)
+
+    p[p <= 0.9] = 0
+    p[p > 0.9] = 255
+    re_mask = uncrop_image_v2(p,ri)
+    re_img = uncrop_image_v2(c_img,ri)
+    
+    return re_img, re_mask
+
+def mask_flattened(c_msk):
+    # Create an empty array with the same shape as the first three dimensions of c_msk
+    result = np.zeros(c_msk.shape[:-1])
+    
+    # Set the value of result to 1 where any layer of c_msk is active
+    result[np.any(c_msk > 0, axis=-1)] = 1
+    
+    return np.expand_dims(result,len(result.shape))
+
+def predict_colors(re_img,re_msk,N):
+
+    # Load and reshape the image
+    image = re_img * mask_flattened(re_msk)
+    image[image == 255] = 0
+    pixel_vals = image.reshape((-1, 3))
+    pixel_vals = np.float32(pixel_vals)
+
+    # Select only the pixels that are non-zero on all three channels
+    non_zero_pixels = non_zero_pixels = pixel_vals[np.nonzero(np.all(pixel_vals != 0, axis=-1) & np.any(pixel_vals != 255, axis=-1))]
+
+    # Apply k-means clustering with k=3 on the non-zero pixels
+    kmeans = KMeans(n_clusters=N)
+    kmeans.fit(non_zero_pixels)
+    centers = kmeans.cluster_centers_
+    centers = np.uint8(centers)
+
+
+    # Print the RGB values of the three most dominant colors
+    print("The RGB values of the three most dominant colors are:")
+    for i in range(N):
+        print(f"Cluster {i}: {centers[i]}")
+    
+    centers_hex = ['#'+str(rgb_to_hex(tuple(i))) for i in centers]
+    
+    return centers
+
+def black_to_white(image: np.ndarray) -> np.ndarray:
+    # Create a mask for black pixels
+    mask = np.all(image == [0, 0, 0], axis=-1)
+    
+    # Change black pixels to white
+    image[mask] = [255, 255, 255]
+    
+    return image
+
+def get_n_most_common_colors(image_a, n):
+    img = Image.fromarray(image_a)
+    colors = [color for color in img.getcolors(img.size[0] * img.size[1]) if color[1] != (0, 0, 0) and color[1] != (255, 255, 255)]
+    sorted_colors = sorted(colors, key=lambda t: t[0], reverse=True)
+    return [np.array(color[1]) for color in sorted_colors[:n]]
+
+def predict_mask(img,mask,N):
+    centers = get_n_most_common_colors(cv2.bitwise_and(img, img, mask),N)
+    threshold = 32
+    outputs = []
+    for i in range(len(tuple(centers))):
+        rgb_color = tuple(centers[i])
+        lower = np.array([max(0, c - threshold) for c in rgb_color])
+        upper = np.array([min(255, c + threshold) for c in rgb_color])
+        mask = cv2.inRange(img, lower, upper)
+        output = cv2.bitwise_and(img, img, mask=mask)
+        output = black_to_white(output)
+        outputs.append(output)
+    return outputs, centers
+
+#Preprocessing Lib End
 
 
 # Specify canvas parameters in application
@@ -181,5 +377,42 @@ if st.button('Reset line positions'):
         os.remove('prediction_target.jpg')
 
 
+st.markdown("<h1 style='text-align: center;'>Curve Digitizer</h1>", unsafe_allow_html=True)
+bg_image = st.file_uploader("Background image:", type=["png", "jpg"])
+st.markdown("<h2 style='text-align: left;'></h2>", unsafe_allow_html=True)  
+st.markdown("<h2 style='text-align: left;'>Predict Curves</h2>", unsafe_allow_html=True)
+predict_button = st.button('load_model')
+
+if predict_button:
+    host = "https://ai-schlumberger-eag-consulting.p.datascience.cloud.slb-ds.com"
+    apiKey = 'CvyAhYL5Y1AO5VLd1aNDUUVSrgXRBFjL'
+
+    client = dataiku.set_remote_dss(host, apiKey)
+
+    model_folder = dataiku.Folder("Module1",'MODULE1')
+    model_path = model_folder.list_paths_in_partition()
+
+    tmp_file_path = 'better_predictor_v5.h5'
+
+    with open(tmp_file_path, "wb") as tmp_file:
+        with model_folder.get_download_stream(model_path[0]) as model_weight_file:
+            tmp_file.write(model_weight_file.read())
+
+    model = tf.keras.models.load_model(tmp_file_path,compile=False)
+    os.remove(tmp_file_path)
 
 
+    st.success('Model Loaded')
+    model.summary(print_fn=lambda x: st.text(x))
+    image = Image.open(bg_image)
+    st.write(np.asarray(image).shape)
+    re_img,re_mask = predict_curves(np.asarray(image),model)
+    st.success('Model Loaded')
+    st.image(re_mask)
+    N = 10
+    image = re_img
+    threshold = 32 # example threshold value
+    outputs, centers = predict_mask(re_img,re_mask,10)
+    st.write(outputs)
+    
+    
