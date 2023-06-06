f29a8b39573df8f36786e166fffc45fe12f2759e
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-07 00:51:24 +0800

    wada

--- Home.py
+++ Home.py
@@ -16,18 +16,16 @@ css = """
 
 st.markdown(css, unsafe_allow_html=True)
 
-
-with st.tabs(["Description"]):
-        st.markdown("<h2 style='text-align: center;'>Functionality Description 📜</h2>", unsafe_allow_html=True)
-        st.markdown("""<h4 style='text-align: justify;'>The algorithm works by combining DeepLabV3+ images segmentation architecture to segmentize the digital curve by filtering the curves line that are potentially the data points and differentiating between curves using k-mean. 
+st.markdown("<h2 style='text-align: center;'>Functionality Description 📜</h2>", unsafe_allow_html=True)
+st.markdown("""<h4 style='text-align: justify;'>The algorithm works by combining DeepLabV3+ images segmentation architecture to segmentize the digital curve by filtering the curves line that are potentially the data points and differentiating between curves using k-mean. 
                     The ranges of axes are needed for interpolation.</h4>""", unsafe_allow_html=True)
-        st.markdown("""
+st.markdown("""
         <ul>
         <li style="font-size: 20px;"><strong>Input<strong>: Images, Axes-Range</li>
         <li style="font-size: 20px;"><strong>Output<strong>: Choosen data points containing the digitized curve in CSV format</li>
         </ul>
         """, unsafe_allow_html=True)
-        st.markdown("""
+st.markdown("""
             <blockquote style="font-size: 18px;">
                 <i>User will need to manually select the relevant predicted figures.</i>
             </blockquote>
