c04098fb975619d2e60e91b3ed720e98c1b77470
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-07 01:47:58 +0800

    awda

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.set_page_config(layout="wide",page_title="Curve Digitalization",page_icon="📈",)
 
+st.set_page_config(layout="wide",page_title="Curve Digitalization",page_icon="📈",)
+st.image('./dark.png',width=200)
 st.markdown("<h1 style='text-align: center;'>Line Curve Digitalization</h1>", unsafe_allow_html=True)
 
 
