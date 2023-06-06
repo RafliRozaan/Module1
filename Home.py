ad412d4e21de07fc843c70c6bd75b238e9354d20
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-07 02:12:18 +0800

    awdwa

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ col1, col2 = st.columns((1, 8))
 col1.image('http://thepetro.cloud/wp-content/uploads/2023/05/Petrocloud.png',width=100)
 col2.markdown("<h1 style='text-align: left; margin-bottom: 0;'>Line Curve Digitalization</h1>", unsafe_allow_html=True)
 
-st.markdown("<hr style='border-top: 2px solid #4f8bf9; margin-top: 0;'/>", unsafe_allow_html=True)
+st.markdown("<hr style='border-top: 2px solid ; margin-top: 0;'/>", unsafe_allow_html=True)
 
 css = """
         <style>
@@ -18,7 +18,7 @@ css = """
 
 st.markdown(css, unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center; color: #4f8bf9;'>Functionality Description</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: left;'>Functionality Description</h2>", unsafe_allow_html=True)
 st.markdown("""<p style='text-align: justify;'>The algorithm combines DeepLabV3+ image segmentation architecture with k-means clustering to filter and differentiate between curves in the digital image. The ranges of the axes are used for interpolation.</p>""", unsafe_allow_html=True)
 st.markdown("""
         <ul>
