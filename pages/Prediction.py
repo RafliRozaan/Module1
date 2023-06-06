9b23bde8f43bca1a770b08f32477b2cb18eb07b1
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-06 11:09:17 +0800

    124124124

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -21,11 +21,9 @@ realtime_update = st.sidebar.checkbox("Update in realtime", True)
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
-    stroke_color=stroke_color,
     background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
     update_streamlit=realtime_update,
-    height=200,
-    width=300,
     drawing_mode=drawing_mode,
+    height=200,
+    width=200,
 )
