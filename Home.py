a7975402d5be491c0e76ea784863b4801801f433
Rafli Rozaan Zuhdi <kil190705@siswa.um.edu.my>
2023-06-07 02:05:48 +0800

    aadwdwa

--- Home.py
+++ Home.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.set_page_config(layout="wide",page_title="Curve Digitalization",page_icon="📈",)
 
-col1, col2 = st.columns((1,20))
+col1, col2 = st.columns((1, 2))
 col1.image('http://thepetro.cloud/wp-content/uploads/2023/05/Petrocloud.png',width=100)
-col2.markdown("<h1 style='text-align: center;'>Line Curve Digitalization</h1>", unsafe_allow_html=True)
+col2.markdown("<h1 style='text-align: center; color: #4f8bf9;'>Line Curve Digitalization</h1>", unsafe_allow_html=True)
 
-st.markdown("<hr/>", unsafe_allow_html=True)
+st.markdown("<hr style='border-top: 2px solid #4f8bf9;'/>", unsafe_allow_html=True)
 
 css = """
         <style>
@@ -18,18 +18,16 @@ css = """
 
 st.markdown(css, unsafe_allow_html=True)
 
-st.markdown("<h3 style='text-align: center;'>Functionality Description</h3>", unsafe_allow_html=True)
-st.markdown("""<h4 style='text-align: justify;'>The algorithm works by combining DeepLabV3+ images segmentation architecture to segmentize the digital curve by filtering the curves line that are potentially the data points and differentiating between curves using k-mean. 
-                    The ranges of axes are needed for interpolation.</h4>""", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: #4f8bf9;'>Functionality Description</h2>", unsafe_allow_html=True)
+st.markdown("""<p style='text-align: justify;'>The algorithm combines DeepLabV3+ image segmentation architecture with k-means clustering to filter and differentiate between curves in the digital image. The ranges of the axes are used for interpolation.</p>""", unsafe_allow_html=True)
 st.markdown("""
         <ul>
         <li style="font-size: 20px;"><strong>Input<strong>: Images, Axes-Range</li>
-        <li style="font-size: 20px;"><strong>Output<strong>: Choosen data points containing the digitized curve in CSV format</li>
+        <li style="font-size: 20px;"><strong>Output<strong>: Chosen data points containing the digitized curve in CSV format</li>
         </ul>
         """, unsafe_allow_html=True)
-
 st.markdown("""
             <blockquote style="font-size: 18px;">
-                <i>User will need to manually select the relevant predicted figures.</i>
+                <i>The user will need to manually select the relevant predicted figures.</i>
             </blockquote>
             """, unsafe_allow_html=True)
